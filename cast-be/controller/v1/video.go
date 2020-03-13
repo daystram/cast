2370c77006d90401229b944c709986efdd68de91
Danny August Ramaputra <daystram@gmail.com>
2020-03-13 14:36:16 +1000

    initialized controllers, and created video retrieval endpoint

---
+++ cast-be/controller/v1/video.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	"fmt"
+	"gitlab.com/daystram/cast/cast-be/datatransfers"
+	"gitlab.com/daystram/cast/cast-be/handlers"
+
+	"github.com/astaxie/beego"
+)
+
+// Video Content Controller
+type VideoController struct {
+	beego.Controller
+	Handler handlers.Handler
+}
+
+// @Title Get List
+// @Success 200 {object} models.Object
+// @Param   variant		query	string	true	"variant"
+// @Param   count		query   int 	false 8	"count"
+// @Param   offset		query   int 	false 0	"offset"
+// @router /fresh [get]
+func (c *VideoController) GetList(variant string, count, offset int) datatransfers.Response {
+	videos, err := c.Handler.GetVideo(variant, count, offset)
+	if err != nil {
+		fmt.Printf("[VideoController::GetList] failed retrieving fresh videos. %+v\n", err)
+		return datatransfers.Response{Error: "Failed retrieving fresh videos", Code: 500}
+	}
+	return datatransfers.Response{Data: videos, Code: 200}
+}
+
+// @Title Search
+// @Success 200 {object} models.Object
+// @router /search [get]
+func (c *VideoController) Search() {
+
+}
+
+// @Title Play Live
+// @Success 200 {object} models.Object
+// @router /live [get]
+func (c *VideoController) PlayLive() {
+
+}
+
+// @Title Play Video
+// @Success 200 {object} models.Object
+// @router /video [get]
+func (c *VideoController) PlayVideo() {
+
+}
