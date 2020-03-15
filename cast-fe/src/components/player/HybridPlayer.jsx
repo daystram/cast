5b845424617d003cbaf2d8d4154fd5e4ef9583db
Danny August Ramaputra <daystram@gmail.com>
2020-03-15 18:40:21 +1000

    Fixed cors policy

--- cast-fe/src/components/player/HybridPlayer.jsx
+++ cast-fe/src/components/player/HybridPlayer.jsx
@@ -34,7 +34,7 @@ class HybridPlayer extends React.Component {
       flvjs: {
         mediaDataSource: {
           isLive: true,
-          cors: true,     // TODO: NOTICE!
+          cors: false,     // TODO: NOTICE!
           withCredentials: false,
         },
       },
