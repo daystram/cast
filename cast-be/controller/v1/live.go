0b9f76ad986ec35b176f1c03b7dd7dccd9e45df9
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:21:37 +1000

    Added live stream endpoints

--- cast-be/controller/v1/live.go
+++ cast-be/controller/v1/live.go
@@ -2,10 +2,14 @@ package v1
 
 import (
 	"fmt"
-	"github.com/astaxie/beego"
-	"gitlab.com/daystram/cast/cast-be/datatransfers"
+	"gitlab.com/daystram/cast/cast-be/constants"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
 
+	"gitlab.com/daystram/cast/cast-be/datatransfers"
 	"gitlab.com/daystram/cast/cast-be/handlers"
+
+	"github.com/astaxie/beego"
 )
 
 // Live Stream Upstream Controller
@@ -16,11 +20,33 @@ type LiveController struct {
 
 // @Title Receive RTMP UpLink
 // @Success 200 {object} models.Object
-// @router /:username  [get]
-func (c *LiveController) PlayLive(username string) datatransfers.Response{
+// @router /stream/:username  [get]
+func (c *LiveController) PlayLive(username string) datatransfers.Response {
 	if err := c.Handler.StreamLive(username, c.Ctx.ResponseWriter, c.Ctx.Request); err != nil {
 		fmt.Printf("[LiveController::PlayLive] failed playing live stream from %s. %+v\n", username, err)
-		return datatransfers.Response{Error: "failed playing live stream", Code:  500}
+		return datatransfers.Response{Error: "failed playing live stream", Code: http.StatusInternalServerError}
+	}
+	return datatransfers.Response{}
+}
+
+type LiveControllerAuth struct {
+	beego.Controller
+	Handler handlers.Handler
+	userID  primitive.ObjectID
+}
+
+func (c *LiveControllerAuth) Prepare() {
+	c.userID, _ = primitive.ObjectIDFromHex(c.Ctx.Input.Param(constants.ContextParamUserID))
+}
+
+// @Title Receive RTMP UpLink
+// @Success 200 {object} models.Object
+// @Param   open		query	bool	true	"open"
+// @router /window  [get]
+func (c *LiveControllerAuth) ControlWindow(open bool) datatransfers.Response {
+	if err := c.Handler.ControlUpLinkWindow(c.userID, open); err != nil {
+		fmt.Printf("[LiveController::ControlWindow] failed setting stream window for %s. %+v\n", c.userID, err)
+		return datatransfers.Response{Error: "failed setting stream window", Code: http.StatusInternalServerError}
 	}
 	return datatransfers.Response{}
 }
