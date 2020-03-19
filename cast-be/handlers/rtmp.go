84c8654f7b24f346c0cfaa58a93d1e148a5583e4
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:21:53 +1000

    Integrated RTMP stream server

--- cast-be/handlers/rtmp.go
+++ cast-be/handlers/rtmp.go
@@ -3,15 +3,18 @@ package handlers
 import (
 	"errors"
 	"fmt"
-	"github.com/nareix/joy4/av/avutil"
-	"github.com/nareix/joy4/av/pubsub"
-	"github.com/nareix/joy4/format/flv"
-	"github.com/nareix/joy4/format/rtmp"
-	"gitlab.com/daystram/cast/cast-be/config"
 	"io"
 	"net/http"
 	"path"
 	"sync"
+
+	"gitlab.com/daystram/cast/cast-be/config"
+
+	"github.com/nareix/joy4/av/avutil"
+	"github.com/nareix/joy4/av/pubsub"
+	"github.com/nareix/joy4/format/flv"
+	"github.com/nareix/joy4/format/rtmp"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type writeFlusher struct {
@@ -37,7 +40,7 @@ func (m *module) CreateRTMPUpLink() {
 		m.live.mutex.Lock()
 		ch := m.live.streams[path.Base(conn.URL.Path)]
 		if ch == nil {
-			fmt.Printf("[RTMPUpLink] Publish request on %s created\n", path.Base(conn.URL.Path))
+			fmt.Printf("[RTMPUpLink] UpLink for %s connected\n", path.Base(conn.URL.Path))
 			stream, _ := conn.Streams()
 			ch = &Stream{}
 			ch.queue = pubsub.NewQueue()
@@ -45,7 +48,7 @@ func (m *module) CreateRTMPUpLink() {
 			m.live.streams[path.Base(conn.URL.Path)] = ch
 			m.live.mutex.Unlock()
 		} else {
-			fmt.Printf("[RTMPUpLink] Publish request on %s already exists\n", path.Base(conn.URL.Path))
+			fmt.Printf("[RTMPUpLink] UpLink for %s already exists\n", path.Base(conn.URL.Path))
 			m.live.mutex.Unlock()
 			return
 		}
@@ -57,12 +60,17 @@ func (m *module) CreateRTMPUpLink() {
 	fmt.Printf("[CreateRTMPUpLink] RTMP UpLink created at port %d\n", config.AppConfig.RTMPPort)
 }
 
+func (m *module) ControlUpLinkWindow(userID primitive.ObjectID, open bool) (err error) {
+	return m.db().videoOrm.SetLive(userID, open)
+}
+
 func (m *module) StreamLive(_ string, w http.ResponseWriter, r *http.Request) (err error) {
 	m.live.mutex.RLock()
 	ch := m.live.streams[path.Base(r.URL.Path)]
 	m.live.mutex.RUnlock()
 
 	if ch != nil {
+		fmt.Printf("[StreamLive] Streaming request for %s\n", path.Base(r.URL.Path))
 		w.Header().Set("Content-Type", "video/x-flv")
 		w.Header().Set("Transfer-Encoding", "chunked")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -74,6 +82,7 @@ func (m *module) StreamLive(_ string, w http.ResponseWriter, r *http.Request) (e
 			ch.queue.Latest(),
 		)
 	} else {
+		fmt.Printf("[StreamLive] Unable to find stream for %s\n", path.Base(r.URL.Path))
 		return errors.New(fmt.Sprintf("[StreamLive] Unable to find stream for %s", path.Base(r.URL.Path)))
 	}
 	return
