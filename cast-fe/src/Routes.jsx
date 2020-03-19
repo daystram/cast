32a34c6dde508f529eabb6fbdfc42a11c9a151a6
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:27:43 +1000

    Added login, signup routes, added PublicRoute

--- cast-fe/src/Routes.jsx
+++ cast-fe/src/Routes.jsx
@@ -1,19 +1,37 @@
 import React from "react";
-import {Switch, Redirect, Route} from 'react-router-dom'
-import {Home, Scene} from "./components";
-import Login from "./components/Login";
+import {Redirect, Route, Switch} from 'react-router-dom'
+import {Home, LogOut, Scene} from "./components";
+import Login from "./components/LogIn";
 import SignUp from "./components/SignUp";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path={"/"} exact component={Home}/>
-      <Route path="/w/:_id" exact component={Scene}/>
-      <Route path="/login" exact component={Login}/>
-      <Route path="/signup" exact component={SignUp}/>
+      <Route path="/w/:hash" exact component={Scene}/>
+      <Route path="/logout" component={LogOut}/>
+      {/*<PrivateRoute path="/profile" exact component={Profile}/>*/}
+      <PublicRoute path="/login" exact component={Login}/>
+      <PublicRoute path="/signup" exact component={SignUp}/>
       <Route><Redirect to="/"/></Route>
     </Switch>
   )
 };
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    localStorage.getItem("username")
+      ? <Component {...props} />
+      : <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
+  )}/>
+);
+
+const PublicRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    localStorage.getItem("username")
+      ? <Redirect to={{pathname: '/', state: {from: props.location}}}/>
+      : <Component {...props} />
+  )}/>
+);
 
 export default Routes;
