eccbacaffdf97d70af938cbcdadde1dfc777bd1a
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:24:17 +1000

    Added unique field checks, improved user DB queries

--- cast-be/models/user.go
+++ cast-be/models/user.go
@@ -1,14 +1,24 @@
 package models
 
 import (
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"gitlab.com/daystram/cast/cast-be/config"
 	"gitlab.com/daystram/cast/cast-be/constants"
 	"gitlab.com/daystram/cast/cast-be/datatransfers"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserOrmer interface {
-	GetOneByID(email string) (datatransfers.User, error)
+	GetOneByID(ID primitive.ObjectID) (user datatransfers.User, err error)
+	GetOneByUsername(username string) (user datatransfers.User, err error)
+	CheckUnique(field, value string) (err error)
+	InsertUser(user datatransfers.User) (ID primitive.ObjectID, err error)
 }
 
 type userOrm struct {
@@ -19,8 +29,34 @@ func NewUserOrmer(db *mongo.Client) UserOrmer {
 	return &userOrm{db.Database(config.AppConfig.MongoDBName).Collection(constants.DBCollectionUser)}
 }
 
-func (o *userOrm) GetOneByID(ID string) (datatransfers.User, error) {
-	return datatransfers.User{}, nil
+func (o *userOrm) GetOneByID(ID primitive.ObjectID) (user datatransfers.User, err error) {
+	err = o.collection.FindOne(context.TODO(), bson.M{"_id": ID}).Decode(&user)
+	return
 }
 
+func (o *userOrm) GetOneByUsername(username string) (user datatransfers.User, err error) {
+	err = o.collection.FindOne(context.TODO(), bson.M{"username": username}).Decode(&user)
+	return
+}
 
+func (o *userOrm) CheckUnique(field, value string) (err error) {
+	uniqueOptions := &options.FindOneOptions{Collation: &options.Collation{Locale: "en", Strength: 2}}
+	if err = o.collection.FindOne(context.TODO(), bson.M{field: value}, uniqueOptions).Err(); err == mongo.ErrNoDocuments {
+		return nil
+	}
+	if err == nil {
+		return errors.New("[CheckUnique] duplicate entry found")
+	}
+	return
+}
+
+func (o *userOrm) InsertUser(user datatransfers.User) (ID primitive.ObjectID, err error) {
+	result := &mongo.InsertOneResult{}
+	if user.ID.IsZero() {
+		user.ID = primitive.NewObjectID()
+	}
+	if result, err = o.collection.InsertOne(context.TODO(), user); err != nil {
+		return
+	}
+	return result.InsertedID.(primitive.ObjectID), nil
+}
