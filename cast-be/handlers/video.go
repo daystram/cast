00874fd4dc97f29efa9107881a9865cadb78364e
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:22:51 +1000

    Added video view counters

--- cast-be/handlers/video.go
+++ cast-be/handlers/video.go
@@ -3,13 +3,14 @@ package handlers
 import (
 	"errors"
 	"fmt"
+	"gitlab.com/daystram/cast/cast-be/constants"
 
 	data "gitlab.com/daystram/cast/cast-be/datatransfers"
 )
 
-func (m *module) GetVideo(variant string, count int, offset int) (videos []data.Video, err error) {
+func (m *module) VideoList(variant string, count int, offset int) (videos []data.Video, err error) {
 	if videos, err = m.db().videoOrm.GetRecent(variant, count, offset); err != nil {
-		return nil, errors.New(fmt.Sprintf("[VideoHandler] error retrieving recent videos. %+v", err))
+		return nil, errors.New(fmt.Sprintf("[VideoList] error retrieving recent videos. %+v", err))
 	}
 	return
 }
@@ -18,10 +19,15 @@ func (m *module) Search(query string, tags []string) (videos []data.Video, err e
 	return nil, nil
 }
 
-func (m *module) VODDetails(hash string) (video data.Video, err error) {
-	return data.Video{}, nil
-}
-
-func (m *module) LiveDetails(username string) (video data.Video, err error) {
-	return data.Video{}, nil
+func (m *module) VideoDetails(hash string) (video data.Video, err error) {
+	if video, err = m.db().videoOrm.GetOneByHash(hash); err != nil {
+		return data.Video{}, errors.New(fmt.Sprintf("[VideoDetails] video with hash %s not found. %+v", hash, err))
+	}
+	video.Views++
+	if video.Type == constants.VideoTypeVOD {
+		if err = m.db().videoOrm.IncrementViews(hash); err != nil {
+			return data.Video{}, errors.New(fmt.Sprintf("[VideoDetails] failed incrementing views of %s. %+v", hash, err))
+		}
+	}
+	return
 }
