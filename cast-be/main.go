05b37b368f7952a4998ac4b5024ea7766422e39f
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:18:47 +1000

    Exposed auth headers

--- cast-be/main.go
+++ cast-be/main.go
@@ -11,9 +11,11 @@ func main() {
 	//_ = logs.SetLogger(logs.AdapterFile, `{"filename":"logs/test.log", "separate":["error"], "level": 3}`)
 
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		ExposeHeaders:    []string{"*", "Authorization"},
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Authorization", "Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
 		AllowCredentials: true,
 	}))
-
 	beego.Run()
 }
