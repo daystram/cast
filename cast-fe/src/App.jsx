054bdc02145446f4f4b6ff45b0cc7909549b7e1a
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:25:39 +1000

    Added axios interceptors

--- cast-fe/src/App.jsx
+++ cast-fe/src/App.jsx
@@ -3,6 +3,29 @@ import './App.css';
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './Routes'
 import Navigation from "./components/Navigation";
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:8080/api/v1";
+axios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+axios.interceptors.response.use(
+  response => {
+    const token = response.headers.authorization; // must be lowercase
+    if (token) {
+      localStorage.setItem("auth_token", token);
+    }
+    return response;
+  },
+  error => Promise.reject(error)
+);
 
 function App() {
   return (
