a7fc34da3b797b27921ff86f2eca0a73db655f93
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:26:29 +1000

    Added abbreviator for time

--- cast-fe/src/helper/abbreviate.js
+++ cast-fe/src/helper/abbreviate.js
@@ -1,15 +1,47 @@
-export default function abbreviate(value) {
-  if (value >= 10 ** 9) {
-    value /= 10 ** 9;
-    return Math.round(value * 10) / 10 + 'b';
+export default function abbreviate() {
+  return {
+    number: (value) => {
+      if (value >= 10 ** 9) {
+        value /= 10 ** 9;
+        return Math.round(value * 10) / 10 + 'b';
+      }
+      if (value >= 10 ** 6) {
+        value /= 10 ** 6;
+        return Math.round(value * 10) / 10 + 'm';
+      }
+      if (value >= 10 ** 3) {
+        value /= 10 ** 3;
+        return Math.round(value * 10) / 10 + 'k';
+      }
+      return value
+    },
+    time: (diff) => {
+      diff = Math.round(diff / 1000);
+      if (diff > 29030400) {
+        diff = Math.round(diff / 29030400);
+        return `${diff} year${diff === 1 ? "" : "s"}`
+      }
+      if (diff > 2419200) {
+        diff = Math.round(diff / 2419200);
+        return `${diff} month${diff === 1 ? "" : "s"}`
+      }
+      if (diff > 604600) {
+        diff = Math.round(diff / 604600);
+        return `${diff} week${diff === 1 ? "" : "s"}`
+      }
+      if (diff > 86400) {
+        diff = Math.round(diff / 86400);
+        return diff === 1 ? "Yesterday" : `${diff} days`
+      }
+      if (diff > 3600) {
+        diff = Math.round(diff / 3600);
+        return `${diff} hour${diff === 1 ? "" : "s"}`
+      }
+      if (diff > 60) {
+        diff = Math.round(diff / 60);
+        return `${diff} minute${diff === 1 ? "" : "s"}`
+      }
+      return `${diff} second${diff === 1 ? "" : "s"}`
+    }
   }
-  if (value >= 10 ** 6) {
-    value /= 10 ** 6;
-    return Math.round(value * 10) / 10 + 'm';
-  }
-  if (value >= 10 ** 3) {
-    value /= 10 ** 3;
-    return Math.round(value * 10) / 10 + 'k';
-  }
-  return value
 }
