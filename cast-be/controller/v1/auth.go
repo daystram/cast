ee3aaca4c982e4a8ddd56110dd8f31737ba199f1
Danny August Ramaputra <daystram@gmail.com>
2020-03-19 21:19:23 +1000

    Added auth handler and controllers

--- cast-be/controller/v1/auth.go
+++ cast-be/controller/v1/auth.go
@@ -1,13 +1,76 @@
 package v1
 
 import (
-	"github.com/astaxie/beego"
+	"fmt"
+	"gitlab.com/daystram/cast/cast-be/errors"
+	"log"
+	"net/http"
+	"strings"
 
+	"gitlab.com/daystram/cast/cast-be/datatransfers"
 	"gitlab.com/daystram/cast/cast-be/handlers"
+
+	"github.com/astaxie/beego"
 )
 
-// Authentication Controller
 type AuthController struct {
 	beego.Controller
 	Handler handlers.Handler
+}
+
+// @Title Register
+// @Param   info    body	{datatransfers.UserRegister}	true	"registration info"
+// @Success 200 success
+// @router /signup [post]
+func (c *AuthController) PostRegister(info datatransfers.UserRegister) datatransfers.Response {
+	err := c.Handler.Register(info)
+	if err != nil {
+		log.Printf("[PostAuthenticate] failed registering %s. %+v\n", info.Username, err)
+		return datatransfers.Response{Error: "failed registering", Code: http.StatusInternalServerError}
+	}
+	return datatransfers.Response{Code: http.StatusOK}
+}
+
+// @Title Check Field
+// @Param   info    body	{datatransfers.UserFieldCheck}	true	"user field info"
+// @Success 200 success
+// @router /check [post]
+func (c *AuthController) PostCheckUnique(info datatransfers.UserFieldCheck) datatransfers.Response {
+	err := c.Handler.CheckUniqueUserField(info.Field, info.Value)
+	if err != nil {
+		log.Printf("[PostCheckUnnique] user %s field already used. %+v\n", info.Field, err)
+		return datatransfers.Response{Error: fmt.Sprintf("%s already used", strings.Title(info.Field)), Code: http.StatusConflict}
+	}
+	return datatransfers.Response{Code: http.StatusOK}
+}
+
+// @Title Login
+// @Param   info    body	{datatransfers.UserLogin}	true	"login info"
+// @Success 200 success
+// @router /login [post]
+func (c *AuthController) PostAuthenticate(info datatransfers.UserLogin) datatransfers.Response {
+	fmt.Println(info)
+	token, err := c.Handler.Authenticate(info)
+	switch err {
+	case nil:
+		c.Ctx.Output.Header("Authorization", fmt.Sprintf("Bearer %s", token))
+		return datatransfers.Response{Data: fmt.Sprintf("Bearer %s", token), Code: http.StatusOK}
+	case errors.ErrNotRegistered:
+		log.Printf("[PostAuthenticate] failed authenticating %s. %+v\n", info.Username, err)
+		return datatransfers.Response{Error: "Username not registered", Code: http.StatusNotFound}
+	case errors.ErrIncorrectPassword:
+		log.Printf("[PostAuthenticate] failed authenticating %s. %+v\n", info.Username, err)
+		return datatransfers.Response{Error: "Incorrect password", Code: http.StatusForbidden}
+	default:
+		log.Printf("[PostAuthenticate] failed authenticating %s. %+v\n", info.Username, err)
+		return datatransfers.Response{Error: "Username not registered", Code: http.StatusNotFound}
+	}
+}
+
+// @Title Logout
+// @Success 200 success
+// @router /logout [post]
+func (c *AuthController) PostDeAuthenticate() {
+	// TODO: stop stream
+	c.Ctx.Output.Header("Authorization", "")
 }
---
+++ cast-be/handlers/auth.go
@@ -0,0 +1,96 @@
+package handlers
+
+import (
+	"fmt"
+	errors2 "gitlab.com/daystram/cast/cast-be/errors"
+	"time"
+
+	"gitlab.com/daystram/cast/cast-be/config"
+	"gitlab.com/daystram/cast/cast-be/constants"
+	"gitlab.com/daystram/cast/cast-be/datatransfers"
+
+	"github.com/astaxie/beego/orm"
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (m *module) CheckUniqueUserField(field, value string) (err error) {
+	return m.db().userOrm.CheckUnique(field, value)
+}
+
+func (m *module) Register(info datatransfers.UserRegister) (err error) {
+	if err = m.db().userOrm.CheckUnique("Name", info.Name); err != nil {
+		fmt.Printf("[Register] Name %s already exists. %+v\n", info.Name, err)
+		return
+	}
+	if err = m.db().userOrm.CheckUnique("Username", info.Username); err != nil {
+		fmt.Printf("[Register] Username %s already exists. %+v\n", info.Username, err)
+		return
+	}
+	if err = m.db().userOrm.CheckUnique("Email", info.Email); err != nil {
+		fmt.Printf("[Register] Email %s already exists. %+v\n", info.Email, err)
+		return
+	}
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(info.Password), bcrypt.DefaultCost)
+	var userID primitive.ObjectID
+	if userID, err = m.db().userOrm.InsertUser(datatransfers.User{
+		Name:      info.Name,
+		Username:  info.Username,
+		Email:     info.Email,
+		Password:  string(hashed),
+		CreatedAt: time.Now(),
+	}); err != nil {
+		fmt.Printf("[Register] Failed adding %s user entry. %+v\n", info.Username, err)
+		return
+	}
+	if _, err = m.db().videoOrm.InsertVideo(datatransfers.VideoInsert{
+		ID:          primitive.NewObjectID(),
+		Hash:        info.Username,
+		Type:        constants.VideoTypeLive,
+		Author:      userID,
+		Title:       fmt.Sprintf("%s's Livestream", info.Name),
+		Description: "",
+		IsLive:      false,
+		CreatedAt:   time.Now(),
+	}); err != nil {
+		fmt.Printf("[Register] Failed adding %s live video entry. %+v\n", info.Username, err)
+		return
+	}
+
+	return
+}
+
+func (m *module) Authenticate(info datatransfers.UserLogin) (token string, err error) {
+	var user datatransfers.User
+	if user, err = m.validate(info); err != nil {
+		return
+	}
+	if token, err = m.generateToken(user); err != nil {
+		return
+	}
+	return
+}
+
+func (m *module) validate(info datatransfers.UserLogin) (user datatransfers.User, err error) {
+	db := m.db()
+	if user, err = db.userOrm.GetOneByUsername(info.Username); err != nil {
+		if err == orm.ErrNoRows {
+			return datatransfers.User{}, errors2.ErrNotRegistered
+		}
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(info.Password)); err != nil {
+		return datatransfers.User{}, errors2.ErrIncorrectPassword
+	}
+	return
+}
+
+func (m *module) generateToken(user datatransfers.User) (tokenString string, err error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":     user.ID,
+		"expiry": time.Now().Add(time.Hour).Unix(),
+	})
+	tokenString, _ = token.SignedString([]byte(config.AppConfig.JWTSecret))
+	return
+}
