eeb861aa5f13baed7b0133fef9017ff35ba774d2
Danny August Ramaputra <daystram@gmail.com>
2020-03-20 19:26:31 +1000

    Fixed wrong endpoint root

--- cast-fe/src/helper/url.js
+++ cast-fe/src/helper/url.js
@@ -4,7 +4,7 @@ export default function urls() {
     thumbnail: (hash) => `${base}/thumbnail/${hash}.jpg`,
     profile: (username) => `${base}/profile/${username}.png`,
     vod: (hash) => `${base}/${hash}/manifest.mpd`,
-    live: (username) => `http://localhost:8080/api/v1/live/stream/${username}`,
+    live: (username) => `/api/v1/live/stream/${username}`,
     upload: () => `/p/video/upload`,
     list: () => `/video/list`,
   }
