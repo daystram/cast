afba050dc15012b233a248646e91802d70d70574
Danny August Ramaputra <daystram@gmail.com>
2020-03-21 11:13:40 +1000

    Added video edit and delete actions

--- cast-fe/src/components/CastEditable.jsx
+++ cast-fe/src/components/CastEditable.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Badge, Button, Card, Col, Form, Image, Row, Spinner} from "react-bootstrap";
+import {Alert, Badge, Button, Card, Col, Form, Image, Modal, Row, Spinner} from "react-bootstrap";
 import urls from "../helper/url";
 import format from "../helper/format";
 import axios from "axios";
 import {Prompt} from "react-router-dom";
 
 const resolutions = ["Processing", "180p", "360p", "480p", "720p", "1080p"];
+let timeout = null;
 
 class CastEditable extends Component {
   constructor(props) {
@@ -18,14 +19,18 @@ class CastEditable extends Component {
       error_tags: "",
       error_description: "",
       error_edit: "",
+      error_delete: "",
       before: {},
       editing: false,
-      loading: false
+      loading_edit: false,
+      loading_delete: false,
+      prompt: false,
     };
     this.pressEdit = this.pressEdit.bind(this);
     this.pressDelete = this.pressDelete.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.submitForm = this.submitForm.bind(this);
+    this.deleteVideo = this.deleteVideo.bind(this);
   }
 
   pressEdit() {
@@ -49,10 +54,11 @@ class CastEditable extends Component {
         title: this.state.before.title,
         tags: this.state.before.tags,
         description: this.state.before.description,
+        before: {},
         editing: false
       })
     } else {
-      console.log("delete")
+      this.setState({prompt: true})
     }
   }
 
@@ -70,6 +76,7 @@ class CastEditable extends Component {
           return false;
         }
         this.setState({error_title: ""});
+        if (value.toLowerCase().trim() !== this.state.before.title.toLowerCase().trim()) this.checkAvailability(value);
         return true;
       case "tags":
         if (!value.trim()) {
@@ -90,23 +97,78 @@ class CastEditable extends Component {
     }
   }
 
+  checkAvailability(value) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      axios.get(urls().title_check(), {
+        params: {
+          title: value.trim()
+        }
+      }).then((response) => {
+        if (response.data.code !== 200) {
+          this.setState({error_title: response.data.error});
+        } else {
+          this.setState({error_title: ""});
+        }
+      }).catch((error) => {
+        console.log(error);
+        this.setState({error_edit: "An error has occurred!"});
+      });
+    }, 400)
+  }
+
   submitForm() {
     let ok = true;
-    ok &= this.validate("title", this.state.title);
-    ok &= this.validate("tags", this.state.tags);
-    ok &= this.validate("description", this.state.description);
+    ok &= !this.state.error_title;
+    ok &= !this.state.error_description;
+    ok &= !this.state.error_tags;
     if (!ok) return;
-
-    this.setState({loading: true});
-    axios.post(urls().upload(), {}
+    this.setState({loading_edit: true});
+    axios.put(urls().edit(), {
+        hash: this.props.video.hash,
+        title: this.state.title,
+        description: this.state.description,
+        tags: this.state.tags,
+      }
     ).then((response) => {
-      this.setState({editing: false, loading: false});
-      this.fetchVideos();
+      if (response.data.code === 200) {
+        this.setState({
+          editing: false,
+          loading_edit: false,
+          before: {},
+          error_title: "",
+          error_tags: "",
+          error_description: ""
+        });
+      } else {
+        console.log(response.data);
+        this.setState({error_edit: response.data.error, loading_edit: false});
+      }
     }).catch((error) => {
       console.log(error);
-      this.setState({error_edit: "An error has occurred! Please try again.", loading: false});
+      this.setState({error_edit: "An error has occurred! Please try again.", loading_edit: false});
     });
+  }
 
+  deleteVideo() {
+    this.setState({loading_delete: true});
+    axios.delete(urls().delete(), {
+        params: {
+          hash: this.props.video.hash
+        },
+      }
+    ).then((response) => {
+      if (response.data.code === 200) {
+        this.setState({loading_delete: false, prompt: false});
+        this.props.onDelete();
+      } else {
+        console.log(response.data);
+        this.setState({error_delete: response.data.error, loading_delete: false});
+      }
+    }).catch((error) => {
+      console.log(error);
+      this.setState({error_delete: "An error has occurred! Please try again.", loading_delete: false});
+    });
   }
 
   render() {
@@ -117,6 +179,7 @@ class CastEditable extends Component {
             <Image src={urls().thumbnail(this.props.video.hash)} style={style.thumbnail}/>
           </Col>
           <Col md sm={12} className={"responsive-fold"}>
+            {this.state.error_upload && <Alert variant={"danger"}>{this.state.error_edit}</Alert>}
             {this.state.editing ?
               <Form.Group>
                 <Form.Control name={"title"} value={this.state.title} onBlur={this.handleChange}
@@ -147,16 +210,38 @@ class CastEditable extends Component {
             }
           </Col>
           <Col md={"auto"} sm={12}>
-            <Button variant={"success"} block size={"sm"} onClick={this.pressEdit} style={style.button}>
-              <span className="material-icons">{this.state.editing ? "check" : "edit"}</span></Button>
+            <Button variant={"success"} block size={"sm"} style={style.button} onClick={this.pressEdit}
+                    disabled={this.state.before && (this.state.title === this.state.before.title &&
+                      this.state.tags === this.state.before.tags &&
+                      this.state.description === this.state.before.description)}>
+              {this.state.loading_edit ?
+                <Spinner animation="grow" style={style.spinner}/> :
+                <span className="material-icons">{this.state.editing ? "check" : "edit"}</span>
+              }
+            </Button>
             <Button variant={this.state.editing ? "info" : "danger"} block size={"sm"}
                     onClick={this.pressDelete} style={style.button}>
               <span className="material-icons">{this.state.editing ? "clear" : "delete"}</span></Button>
           </Col>
         </Row>
+        <Modal show={this.state.prompt} size={"md"} centered>
+          <Modal.Header>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Delete Video?
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Are you sure you want to delete <b>{this.state.title}</b>? <b>This action cannot be undone.</b></p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant={"info"} onClick={() => this.setState({prompt: false})}>Cancel</Button>
+            <Button variant={"danger"} onClick={this.deleteVideo}>Delete
+              {" "}{this.state.loading_delete ? <Spinner animation="grow" style={style.spinner}/> : ""}</Button>
+          </Modal.Footer>
+        </Modal>
         <Prompt
-          when={this.state.editing || this.state.loading}
-          message={() =>`Are you sure? You changes will be discarded.`}/>
+          when={this.state.editing || this.state.loading_edit}
+          message={() => `Are you sure? You changes will be discarded.`}/>
       </Card>
     )
   }
