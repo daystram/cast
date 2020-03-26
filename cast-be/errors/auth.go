c148805b9070d5692035b39832a130dd017da628
Danny August Ramaputra <daystram@gmail.com>
2020-03-26 11:04:19 +1000

    Added verification error

--- cast-be/errors/auth.go
+++ cast-be/errors/auth.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrNotRegistered     = errors.New("username not registered")
 	ErrIncorrectPassword = errors.New("password incorrect")
+	ErrNotVerified       = errors.New("user not verified")
+	ErrAlreadyVerified   = errors.New("user already verified")
 )
