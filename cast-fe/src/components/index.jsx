f0e4bfad8ff2a92d2350b785d69bc7e60df2db6a
Danny August Ramaputra <daystram@gmail.com>
2020-03-26 11:08:24 +1000

    Added verification page

--- cast-fe/src/components/index.jsx
+++ cast-fe/src/components/index.jsx
@@ -6,5 +6,6 @@ import LogIn from './LogIn'
 import LogOut from './LogOut'
 import Manage from './Manage'
 import Profile from './Profile'
+import Verify from './Verify'
 
-export {Dashboard, Home, Scene, SignUp, LogIn, LogOut, Manage, Profile}
+export {Dashboard, Home, Scene, SignUp, LogIn, LogOut, Manage, Profile, Verify}
