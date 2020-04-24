25a1f3a2d37df5e33f7b45edb4ad59e4f1babfcc
Danny August Ramaputra <daystram@gmail.com>
2020-04-24 19:16:38 +1000

    Fixed auth cookie name, fixed url cdn host

--- cast-fe/src/helper/auth.js
+++ cast-fe/src/helper/auth.js
@@ -1,18 +1,20 @@
 import Cookies from "js-cookie";
 
+const COOKIE_KEY = "cast";
+
 export default function auth() {
   return {
     is_authenticated: () => {
-      return Cookies.get("user")
+      return Cookies.get(COOKIE_KEY)
     },
     username: () => {
-      return Cookies.get("user") ? Cookies.get("user").split("|")[0] : "";
+      return Cookies.get(COOKIE_KEY) ? Cookies.get(COOKIE_KEY).split("|")[0] : "";
     },
     token: () => {
-      return Cookies.get("user") ? Cookies.get("user").split("|")[1] : "";
+      return Cookies.get(COOKIE_KEY) ? Cookies.get(COOKIE_KEY).split("|")[1] : "";
     },
     deauthenticate: () => {
-      return Cookies.remove("user")
+      return Cookies.remove(COOKIE_KEY)
     }
   }
 }
