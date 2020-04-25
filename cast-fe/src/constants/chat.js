96d02757c21dbbefbf55f3081f4bc4a9b4909f2e
Danny August Ramaputra <daystram@gmail.com>
2020-04-25 14:55:12 +1000

    Updatd constants

---
+++ cast-fe/src/constants/chat.js
@@ -0,0 +1,1 @@
+export const CHAT_CHAR_LIMIT = 30;
---
+++ cast-fe/src/constants/file.js
@@ -0,0 +1,6 @@
+const KB = 1000;
+const MB = 1000 * KB;
+const GB = 1000 * MB;
+export const PROFILE_MAX_SIZE = 15 * MB;
+export const THUMBNAIL_MAX_SIZE = 50 * MB;
+export const VIDEO_MAX_SIZE = 1.25 * GB;
