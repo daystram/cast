8cc72a06f3f40e3a4fd13848e93d2e03d44b9c78
Danny August Ramaputra <daystram@gmail.com>
2020-05-13 20:30:37 +1000

    Added email consts for password reset

--- cast-be/constants/email.go
+++ cast-be/constants/email.go
@@ -3,4 +3,7 @@ package constants
 const (
 	EmailTemplateVerification = "email-verify"
 	EmailLinkVerification     = "https://%s/verify?key=%s"
+
+	EmailTemplateReset = "email-reset"
+	EmailLinkReset     = "https://%s/forget?key=%s"
 )
