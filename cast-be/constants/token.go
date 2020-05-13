1c11b74dd1c9142178485e6ff770022e360706c1
Danny August Ramaputra <daystram@gmail.com>
2020-05-13 20:31:07 +1000

    Added token structs and consts

---
+++ cast-be/constants/token.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	TokenHashCharacters    = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	TokenHashDefaultLength = 12
+
+	TokenPurposeVerification = "verify"
+	TokenPurposeReset        = "reset"
+)
