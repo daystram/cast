a38a0410f1be15883518f5102809c6a4b7e53cd1
Danny August Ramaputra <daystram@gmail.com>
2020-05-16 12:41:25 +1000

    Added dynamic list for search results

--- cast-fe/src/components/List.jsx
+++ cast-fe/src/components/List.jsx
@@ -12,18 +12,40 @@ class List extends Component {
     this.state = {
       page: 0,
       casts: [],
+      finished: false,
     };
     this.fetchVideos = this.fetchVideos.bind(this);
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.search && this.props.query !== prevProps.query) {
+      this.setState({casts: [], finished: false, page: 0});
+    }
+  }
+
   fetchVideos() {
-    axios.get(urls().list(), {
-      params: {
-        variant: this.props.variant,
-        count: VIDEO_LIST_PAGE_SIZE,
-        offset: VIDEO_LIST_PAGE_SIZE * this.state.page,
+    let target;
+    let config;
+    if (this.props.search) {
+      target = urls().search();
+      config = {
+        params: {
+          query: this.props.query.trim(),
+          count: VIDEO_LIST_PAGE_SIZE,
+          offset: VIDEO_LIST_PAGE_SIZE * this.state.page,
+        }
+      };
+    } else {
+      target = urls().list();
+      config = {
+        params: {
+          variant: this.props.variant.trim(),
+          count: VIDEO_LIST_PAGE_SIZE,
+          offset: VIDEO_LIST_PAGE_SIZE * this.state.page,
+        }
       }
-    }).then((response) => {
+    }
+    axios.get(target, config).then((response) => {
       if (response.data.code === 200) {
         if (!response.data.data) {
           this.setState({finished: true});
@@ -46,20 +68,20 @@ class List extends Component {
         hasMore={!this.state.finished}
         loadMore={this.fetchVideos}
         loader={
-          <div style={{display: "flex"}}>
+          <div style={{display: "flex"}} key={""}>
             <Spinner style={style.spinner} animation="grow" variant="primary"/>
           </div>
         }
         useWindow={true}
         initialLoad={true}>
         <Row noGutters>
-          {this.state.casts.length ? this.state.casts.map(video =>
+          {!!this.state.casts.length ? this.state.casts.map(video =>
             <Col xl={3} lg={4} md={6} sm={12} key={video.hash} style={{padding: "0 8px 16px 8px"}}>
               <Cast video={video}/></Col>
-          ) : <h5 style={style.h5}>No casts available!</h5>}
-          {this.state.casts.length && this.state.finished &&
+          ) : <h5 style={style.h5}>{this.props.emptyMessage || "No casts available!"}</h5>}
+          {!!this.state.casts.length && this.state.finished &&
           <div style={{width: "100%", margin: "32px 64px"}}>
-            <h5 style={{...style.h5, textAlign: "center"}}>No more casts!</h5>
+            <h5 style={{...style.h5, textAlign: "center"}}>{this.props.finishedMessage || "No more casts!"}</h5>
           </div>}
         </Row>
       </InfiniteScroll>
