205afb1295d0ecc122b7a35072601d2f5ffa2631
Danny August Ramaputra <daystram@gmail.com>
2021-01-30 14:54:51 +1000

    ci: updated gitlab-ci

--- scripts/init_ingest.sh
+++
@@ -1,5 +0,0 @@
-cd cast-is
-export GOPATH=${CI_PROJECT_DIR}/go
-mkdir -p ${GOPATH}/src/${GOPROJECT}/cast-is
-mv * ${GOPATH}/src/${GOPROJECT}/cast-is > /dev/null || true
-cd ${GOPATH}/src/${GOPROJECT}/cast-is
