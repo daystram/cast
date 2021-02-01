1314a6c111a321b242e76df6814c051043a1c31f
Danny August Ramaputra <daystram@gmail.com>
2021-02-01 12:07:29 +1000

    feat: added user registration from OID

--- cast-be/controller/v1/auth.go
+++ cast-be/controller/v1/auth.go
@@ -14,3 +14,17 @@ type AuthControllerAuth struct {
 	beego.Controller
 	Handler handlers.Handler
 }
+
+// @Title Register user from OID, skip if already exists
+// @Param   idToken    body	{datatransfers.UserRegister}	true	"registration info"
+// @Success 200 success
+// @router /check [post]
+func (c *AuthControllerAuth) PostCheckRegister(idToken datatransfers.UserRegister) datatransfers.Response {
+	err := c.Handler.Register(idToken)
+	if err != nil {
+		log.Printf("[AuthControllerAuth::PostCheckRegister] failed registering. %+v\n", err)
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusInternalServerError)
+		return datatransfers.Response{Error: "failed registering", Code: http.StatusInternalServerError}
+	}
+	return datatransfers.Response{Code: http.StatusOK}
+}
