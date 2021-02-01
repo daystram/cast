a47db44e8ff073956841cd90e9f0cf0b6ae9cd1c
Danny August Ramaputra <daystram@gmail.com>
2021-02-01 12:02:37 +1000

    refactor!: updated user registration handler

--- cast-be/handlers/auth.go
+++ cast-be/handlers/auth.go
@@ -2,9 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"github.com/daystram/cast/cast-be/config"
-	"github.com/daystram/cast/cast-be/util"
-	"golang.org/x/crypto/bcrypt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -14,60 +11,58 @@ import (
 	"github.com/daystram/cast/cast-be/datatransfers"
 )
 
-
-func (m *module) Register(info datatransfers.UserRegister) (err error) {
-	if err = m.db.userOrm.CheckUnique("Name", info.Name); err != nil {
-		fmt.Printf("[Register] Name %s already exists. %+v\n", info.Name, err)
+func (m *module) Register(idToken datatransfers.UserRegister) (err error) {
+	var user datatransfers.User
+	if user, err = parseIDToken(idToken.IDToken); err != nil {
+		fmt.Printf("[Register] Failed parsing id_token. %+v\n", err)
 		return
 	}
-	if err = m.db.userOrm.CheckUnique("Username", info.Username); err != nil {
-		fmt.Printf("[Register] Username %s already exists. %+v\n", info.Username, err)
+	if err = m.db.userOrm.CheckUnique("_id", user.ID); err != nil {
+		return nil // user exists
+	}
+	if err = m.db.userOrm.CheckUnique("username", user.Username); err != nil {
+		return nil // user exists
+	}
+	if err = m.db.userOrm.InsertUser(user); err != nil {
+		fmt.Printf("[Register] Failed adding %s user entry. %+v\n", user.Username, err)
 		return
 	}
-	if err = m.db.userOrm.CheckUnique("Email", info.Email); err != nil {
-		fmt.Printf("[Register] Email %s already exists. %+v\n", info.Email, err)
-		return
-	}
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(info.Password), bcrypt.DefaultCost)
-	var userID primitive.ObjectID
-	user := datatransfers.User{
-		Name:      info.Name,
-		Username:  info.Username,
-		Email:     info.Email,
-		Password:  string(hashed),
-		CreatedAt: time.Now(),
-	}
-	if userID, err = m.db.userOrm.InsertUser(user); err != nil {
-		fmt.Printf("[Register] Failed adding %s user entry. %+v\n", info.Username, err)
-		return
-	}
-	user.ID = userID
 	if _, err = m.db.videoOrm.InsertVideo(datatransfers.VideoInsert{
 		ID:          primitive.NewObjectID(),
-		Hash:        info.Username,
+		Hash:        user.Username,
 		Type:        constants.VideoTypeLive,
-		Author:      userID,
-		Title:       fmt.Sprintf("%s's Livestream", info.Name),
+		Author:      user.ID,
+		Title:       fmt.Sprintf("%s's Livestream", user.Username),
 		Tags:        []string{"live", "first"},
 		Description: "Welcome to my stream!",
 		Resolutions: -1,
 		IsLive:      false,
 	}); err != nil {
-		_ = m.db.userOrm.DeleteOneByID(userID)
-		fmt.Printf("[Register] Failed adding %s live video entry. %+v\n", info.Username, err)
+		_ = m.db.userOrm.DeleteOneByID(user.ID)
+		fmt.Printf("[Register] Failed adding %s live video entry. %+v\n", user.Username, err)
 		return
 	}
-	if err = m.SendVerification(user); err != nil {
-		fmt.Printf("[Register] Failed sending %s verification mail. %+v\n", info.Username, err)
-	}
-	_ = util.Copy(
-		fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ProfileRootDir, constants.ProfileDefault),
-		fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ProfileRootDir, user.Username),
-	)
-	_ = util.Copy(
-		fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ThumbnailRootDir, constants.ThumbnailDefault),
-		fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ThumbnailRootDir, user.Username),
-	)
+	// TODO: use S3
+	//_ = util.Copy(
+	//    fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ProfileRootDir, constants.ProfileDefault),
+	//    fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ProfileRootDir, user.Username),
+	//)
+	//_ = util.Copy(
+	//    fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ThumbnailRootDir, constants.ThumbnailDefault),
+	//    fmt.Sprintf("%s/%s/%s.jpg", config.AppConfig.UploadsDirectory, constants.ThumbnailRootDir, user.Username),
+	//)
 	return
 }
 
+func parseIDToken(idToken string) (user datatransfers.User, err error) {
+	claims := jwt.MapClaims{}
+	if _, _, err = new(jwt.Parser).ParseUnverified(idToken, claims); err != nil {
+		return
+	}
+	user = datatransfers.User{
+		ID:        claims["sub"].(string),
+		Username:  claims["preferred_username"].(string),
+		CreatedAt: time.Now(),
+	}
+	return
+}
